223599709cbdf2d431b0e6a2de473e2a5c20819b
yooinbeom <yib950@gmail.com>
2022-02-25 16:22:16 +0900

    Create

---
+++ chapter1/src/test/java/com/example/NavigationTest.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+
+
+public class NavigationTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod() {
+
+        System.setProperty("chrome.driver",
+                "./src/test/resources/drivers/chromedriver");
+
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void navigateToAUrl() {
+
+        driver.get("http://demo-store.seleniumacademy.com/");
+
+        Assert.assertEquals(driver.getTitle(), "Madison Island");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+    	
+        driver.quit();
+    }
+
+}
